5c1ad42c1a3e581edeb5fccdb087a2d24e633402
srilakshmiappu <41777630+srilakshmiappu@users.noreply.github.com>
2018-08-16 14:32:27 +0530

    Update .py

--- .py
+++ .py
@@ -2,10 +2,7 @@ num=8
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+           print("no")
            break
-   else:
-       print(num,"is a prime number")
 else:
-   print(num,"is not a prime number")
+   print("yes")
